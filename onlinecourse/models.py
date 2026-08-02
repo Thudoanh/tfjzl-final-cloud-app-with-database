9800112fd1eb93e5c89012fdcaf68078bad22aec
Thudoanh <anhthudowb2@gmail.com>
2026-08-02 04:05:14 -0400

    Final

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -96,7 +96,7 @@ class Enrollment(models.Model):
 
 # Question model
 class Question(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='questions')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     grade = models.IntegerField(default=50)
 
